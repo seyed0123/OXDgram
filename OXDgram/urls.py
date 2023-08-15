16000784326db9555bec3422fbf2c47548967ad7
seyed0123 <seyed123ali123@gmail.com>
2023-08-15 19:04:22 +0430

    add home page

--- OXDgram/urls.py
+++ OXDgram/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/', include('core.urls'))
+    path('api/', include('core.urls')),
+    path('', home, name='home')
 ]
